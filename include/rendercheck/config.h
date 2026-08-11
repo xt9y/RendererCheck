e82e200b0f83f2019d0ab42e07e32d17afc10907
Felix <felix@xt9y.de>
2026-08-12 00:14:47 +0200

    Add visual test configuration

--- include/rendercheck/config.h
+++ include/rendercheck/config.h
@@ -1,15 +1,25 @@
 #pragma once
 
+#include <cstdint>
 #include <filesystem>
 #include <string>
 #include <vector>
 
 namespace rendercheck {
 
+struct VisualConfig {
+    bool capture = false;
+    std::filesystem::path baseline;
+    std::uint32_t pixel_threshold = 0;
+    double max_changed_percent = 0.0;
+};
+
 struct ProjectConfig {
     std::string name = "renderer";
     std::string command;
     std::filesystem::path cwd = ".";
+    std::filesystem::path baseline_dir = "rendercheck/baselines";
+    VisualConfig visual;
 };
 
 struct TestConfig {
@@ -18,6 +28,7 @@ struct TestConfig {
     std::string args;
     std::filesystem::path cwd;
     bool enabled = true;
+    VisualConfig visual;
 };
 
 struct Config {
